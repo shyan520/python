2fbefa5d34d03344ba56eaddd724b88e07420b96
shyan520 <13564135129@139.com>
2019-06-02 19:18:57 +0800

    初始化Python学习库

---
+++ lesson 1-2.py
@@ -0,0 +1,11 @@
+# 1--1.1.3 Hello Word程序#######################################
+print('Hello Word!')
+
+# 2--2.2.1 变量的命名和使用#####################################
+# 定义变量message
+message = 'Hello Word!'
+print(message)
+
+message = 'Hello Python Crash Course Word!'
+print(message)
+

2df2566ffcb15376066dbaf75b49318ad193c326
shyan520 <yansh520@sina.com>
2019-06-11 11:31:36 +0800

    提交第四章所有代码内容

--- lesson 4.py
+++ lesson 4.py
@@ -62,3 +62,59 @@ print(players[:2])
 print(players[3:])
 print(players[-2:])
 print('4--4.4.1\n')
+
+# 4--4.4.3 复制列表#####################################################################
+my_foods = ['pizza', 'falafel', 'carrot cake']
+# 正确的复制列表方法： 把my_foods整个列表的切片值赋值给新的friend_foods列表
+friend_foods = my_foods[:]
+print(my_foods)
+print(friend_foods)
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print('正确的：' + str(my_foods))
+print('正确的：' + str(friend_foods))
+print('\n')
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+# 错误的复制列表方法： 将新变量 friend_foods 关联到包含在my_foods中的列表，因此这两个变量都指向同一个列表
+friend_foods = my_foods
+print(my_foods)
+print(friend_foods)
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print('错误的：' + str(my_foods))
+print('错误的：' + str(friend_foods))
+print('4--4.4.3\n')
+
+# 4--4.5 元组#########################################################################
+# Python没有数组：
+# 只有元组(tuple)和列表(list)；
+# 元组一旦创建不可改变，例如：aa=tuple(1,2,3)；
+# 元组不能追加(append)元素，弹出(pop)元素等；
+# 只能对元组中的元素进行索引aa[0]，不能对其中的元组进行赋值aa[0]=8；
+# 使用元组的好处在于对元组进行操作更为高效，适合存放一组常量；
+# 而上述的众多不可以，使用列表list是可以的。
+# 4--4.5.1 定义元组###################################################################
+dimensions = (200,50)
+print(dimensions[0])
+print(dimensions[1])
+# 尝试修改第一个元素的值
+# dimensions[0] = 250
+#报错日志：TypeError: 'tuple' object does not support item assignment
+print('4--4.5.1\n')
+
+# 4--4.5.2 遍历元组中的所有值#########################################################
+dimensions = (200,50)
+for dimension in dimensions:
+    print(dimension)
+print('4--4.5.2\n')
+
+# 4--4.5.3 修改元组变量###############################################################
+dimensions = (200,50)
+for dimension in dimensions:
+    print(dimension)
+# 虽然不能修改元组的元素，但可以给存储元组的变量赋值
+dimensions = (250,100,50)
+for dimension in dimensions:
+    print(dimension)
+print('4--4.5.3\n')

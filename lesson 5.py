c3209fb182b5a7e5c65aa1a63dc222b00263e7a2
shyan520 <13564135129@139.com>
2019-06-11 23:01:45 +0800

    初始化lesson5

---
+++ lesson 5.py
@@ -0,0 +1,7 @@
+# 5--5.1 一个简单if语句示例#################################################################
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())

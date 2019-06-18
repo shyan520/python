0586dd9a7a644b3d7345c41c879c467737f0dc0a
shyan520 <13564135129@139.com>
2019-06-18 21:29:55 +0800

    提交第八章代码

--- lesson_8.py
+++ lesson_8.py
@@ -3,7 +3,7 @@ def greet_user():
     print('Hello!')
 greet_user()
 print('8--8.1\n')
-'''
+
 # 8--8.1.1 向函数传递信息###########################################################
 # userName是形参；'yanshunhua'是实参
 def greet_user(userName):
@@ -29,7 +29,7 @@ print('8--8.2.2\n')
 
 # 8--8.2.3 默认值###################################################################
 # 设置sex形参的默认值是'男士'
-def greet_user(userName,sex = '男士'):
+def greet_user(userName,sex='男士'):
     print('Hello! ' + userName + sex)
 # 输入实参的时候，特地缺损sex参数的输入
 greet_user(userName='yanshunhua'.title())
@@ -46,7 +46,7 @@ print('Hello! ' + name)
 print('8--8.3.1\n')
 
 # 8--8.3.3 返回字典#################################################################
-def greet_user(firstName,lastName,age = ''):
+def greet_user(firstName,lastName,age=''):
     user = {'first':firstName,'last':lastName}
     if age:
         user['age'] = age
@@ -92,7 +92,7 @@ for user in users:
     print(user)
 print('8--8.3.4\n')
 
-'''
+
 # 8--8.4.1 在函数中修改列表#########################################################
 # 方案一：
 # 首先创建一个列表，其中包含一些要打印的设计
@@ -142,7 +142,29 @@ print(make_pizza('mushrooms', 'green peppers', 'extra cheese'))
 
 for v in make_pizza('mushrooms', 'green peppers', 'extra cheese'):
     print(v)
+print('8--8.5\n')
 
-# 8--8.5.1 结合使用位置实参和任意数量实参#########################################################
+# 8--8.5.1 结合使用位置实参和任意数量实参############################################
 # 如果要让函数接受不同类型的实参，必须在函数定义中将接纳任意数量实参的形参放在最后
+# 形参中的*号，代表创建了一个空的列表
 def make_pizza(size,*toppings):
+    print('披萨的大小是：' + str(size) + '寸。\n材料有：')
+    for v in toppings:
+        print('- ' + v)
+make_pizza(9,'mushrooms', 'green peppers', 'extra cheese')
+print('8--8.5.1\n')
+
+# 8--8.5.2 使用任意数量的关键字实参###################################################
+# 有时候，需要接受任意数量的实参，但预先不知道传递给函数的会是什么样的信息。在这种情况下，可将函数编写成能够接受任意数量的键—值对——调用语句提供了多少就接受多少。
+# 形参中的**号，代表创建了一个空的字典
+def class_info(grade,className,**classInfo):
+    c_info = {}
+    c_info['年级：'] = grade
+    c_info['班级：'] = className
+    for k,v in classInfo.items():
+        c_info[k] = v
+    return c_info
+
+
+print(class_info('三年级','五班',people_number=48,teacher='李老师'))
+print('8--8.5.2\n')
